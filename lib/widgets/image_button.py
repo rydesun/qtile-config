878d40027397b9a593ceeb915bd778719eebf111
rydesun <rydesun@gmail.com>
2020-03-01 19:18:00 +0800

    Add menu button

---
+++ lib/widgets/image_button.py
@@ -0,0 +1,15 @@
+import subprocess
+
+from libqtile.widget import Image as _Image
+
+
+class ImageButton(_Image):
+    def __init__(self, *args, **kwargs):
+        self.defaults.extend((
+            ("execute", ""),
+        ))
+        super().__init__(*args, **kwargs)
+
+    def button_press(self, x, y, button):
+        if button == 1:
+            subprocess.Popen(self.execute)
