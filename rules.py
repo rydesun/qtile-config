13b10e8acf56ffb3ee959e2e21d9743ecea0ae0c
rydesun <rydesun@gmail.com>
2020-03-11 19:39:39 +0800

    Update window rules

--- rules.py
+++ rules.py
@@ -10,8 +10,20 @@ def _set_window_float(c):
     wm_class = c.window.get_wm_class()
     wm_window_role = c.window.get_wm_window_role()
 
+    if "fcitx-config-gtk3" in wm_class:
+        c.floating = True
+
     if "firefox" in wm_class:
         if wm_window_role == "Organizer" or wm_window_role == "page-info":
             c.floating = True
         elif wm_window_role == "browser":
             pass
+
+    if "lxappearance" in wm_class:
+        c.floating = True
+
+    if "vncviewer" in wm_class:
+        c.floating = True
+
+    if "Wine" in wm_class:
+        c.floating = True
