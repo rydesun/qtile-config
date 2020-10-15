9af5b26527b174a1a3b2da73657ebe7511749ad2
rydesun <rydesun@gmail.com>
2020-10-15 12:02:57 +0800

    Update tasklist style

--- lib/widgets/tasklist.py
+++ lib/widgets/tasklist.py
@@ -13,7 +13,7 @@ class TaskList(_TaskList):
         if not surface:
             return
 
-        x = offset + self.borderwidth + self.padding_x + 2 + self.icon_offset_x
+        x = offset + self.borderwidth + self.padding_x + self.icon_offset_x
         y = self.padding_y + self.borderwidth + self.icon_offset_y
 
         self.drawer.ctx.save()
@@ -21,6 +21,27 @@ class TaskList(_TaskList):
         self.drawer.ctx.set_source(surface)
         self.drawer.ctx.paint()
         self.drawer.ctx.restore()
+
+    def drawbox(self, offset, text, bordercolor, textcolor,
+                width=None, rounded=False, block=False, icon=None):
+        self.drawtext(text, textcolor, width)
+
+        icon_padding = (self.icon_size + self.padding_x) if icon else 0
+        padding_x = [self.padding_x + icon_padding, self.padding_x]
+
+        framed = self.layout.framed(
+            self.borderwidth,
+            bordercolor,
+            padding_x,
+            self.padding_y
+        )
+        if block:
+            framed.draw_fill(offset, self.margin_y, rounded)
+        else:
+            framed.draw_line(offset, self.margin_y, False)
+
+        if icon:
+            self.draw_icon(icon, offset)
 
     def button_press(self, x, y, button):
         if button == 1:
