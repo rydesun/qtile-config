608d379d7f1eccc66faea26b8034400e73864785
rydesun <rydesun@gmail.com>
2020-11-20 11:44:13 +0800

    Swallow terminal emulator

--- rules.py
+++ rules.py
@@ -1,9 +1,11 @@
 from libqtile import hook
-
+import psutil
 
 class RuleMgr:
     def work(self):
         hook.subscribe.client_new(_set_window_float)
+        hook.subscribe.client_new(_swallow)
+        hook.subscribe.client_killed(_unswallow)
 
 
 def _set_window_float(c):
@@ -27,3 +29,22 @@ def _set_window_float(c):
 
     if "Wine" in wm_class:
         c.floating = True
+
+# https://github.com/qtile/qtile/issues/1771#issuecomment-642065762
+def _swallow(window):
+    pid = window.window.get_net_wm_pid()
+    ppid = psutil.Process(pid).ppid()
+    cpids = {c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()}
+    for _ in range(5):
+        if not ppid:
+            return
+        if ppid in cpids:
+            parent = window.qtile.windows_map.get(cpids[ppid])
+            parent.minimized = True
+            window.parent = parent
+            return
+        ppid = psutil.Process(ppid).ppid()
+
+def _unswallow(window):
+    if hasattr(window, 'parent'):
+        window.parent.minimized = False
