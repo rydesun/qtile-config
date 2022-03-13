48419d77f5da8888252c41497cbb36b2bdca3919
rydesun <rydesun@gmail.com>
2022-03-13 19:59:01 +0800

    Load widgets on demand

--- bar.py
+++ bar.py
@@ -1,4 +1,4 @@
-from libqtile import bar
+from libqtile import bar, qtile
 from libqtile import widget as qtile_widget
 from libqtile.command import lazy
 
@@ -10,14 +10,17 @@ class Bar:
         self.env = env
 
     def main_bar(self, theme):
-        return bar.Bar([
+        return bar.Bar(list(filter(lambda w: w is not None, (
             local_widget.Box(
                 image_path=self.env.logo_file,
                 text=self.env.logo_text,
                 mouse_callbacks={
                     "Button1": lazy.spawn(self.env.cmd_menu),
                 },
-                **theme.menu_button),
+                **theme.menu_button)
+            if getattr(self.env, "logo_file", None)
+            or getattr(self.env, "logo_text", None)
+            else None,
 
             qtile_widget.GroupBox(
                 visible_groups=["a", "s", "d", "f"],
@@ -28,13 +31,16 @@ class Bar:
 
             local_widget.TaskList(**theme.tasklist),
 
-            qtile_widget.Systray(**theme.systray),
+            qtile_widget.Systray(**theme.systray)
+            if qtile.core.name == "x11" else None,
 
             qtile_widget.Spacer(length=20),
 
             local_widget.Net(
                 interface=self.env.dev_nic,
-                **theme.netspeed),
+                **theme.netspeed)
+            if getattr(self.env, "dev_nic", None)
+            else None,
 
             local_widget.Battery(
                 low_percentage=0.2,
@@ -45,8 +51,9 @@ class Bar:
                 low_percentage=0.2,
                 update_interval=7,
                 dev_id=self.env.dev_kdeconnect,
-                **theme.kdeconnect,
-            ),
+                **theme.kdeconnect)
+            if getattr(self.env, "dev_kdeconnect", None)
+            else None,
 
             local_widget.Backlight(
                 backlight_name=self.env.dev_backlight,
@@ -54,14 +61,18 @@ class Bar:
                     "Button4": lazy.spawn(self.env.cmd_backlight_increase),
                     "Button5": lazy.spawn(self.env.cmd_backlight_decrease),
                 },
-                **theme.backlight),
+                **theme.backlight)
+            if getattr(self.env, "dev_backlight", None)
+            else None,
 
             local_widget.PulseVolume(
                 **theme.volume),
 
             local_widget.ThermalSensor(
                 dev_name=self.env.dev_thermal,
-                **theme.thermalSensor),
+                **theme.thermalSensor)
+            if getattr(self.env, "dev_thermal", None)
+            else None,
 
             local_widget.Clock(
                 update_interval=0.5,
@@ -70,12 +81,14 @@ class Bar:
             qtile_widget.Wallpaper(
                 wallpaper_command=None,
                 directory=self.env.wallpaper_main_dir,
-                **theme.wallpaper),
+                **theme.wallpaper)
+            if getattr(self.env, "wallpaper_main_dir", None)
+            else None,
 
-        ], **theme.bar)
+        ))), **theme.bar)
 
     def other_bar(self, theme):
-        return bar.Bar([
+        return bar.Bar(list(filter(lambda w: w is not None, (
             qtile_widget.GroupBox(
                 visible_groups=["a", "s", "d", "f", "g"],
                 **theme.groupbox),
@@ -89,6 +102,8 @@ class Bar:
             qtile_widget.Wallpaper(
                 wallpaper_command=None,
                 directory=self.env.wallpaper_other_dir,
-                **theme.wallpaper),
+                **theme.wallpaper)
+            if getattr(self.env, "wallpaper_other_dir", None)
+            else None,
 
-        ], **theme.bar)
+        ))), **theme.bar)
