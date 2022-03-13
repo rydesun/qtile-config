3b19993823fde5791503d93c3596e844f0cad09f
rydesun <rydesun@gmail.com>
2022-03-13 15:37:54 +0800

    Update env and add comments

--- env_example.py
+++ env_example.py
@@ -5,6 +5,7 @@ from libqtile.config import Match
 wallpaper_main_dir = "~/Userdata/Pictures/wallpapers/"
 wallpaper_other_dir = wallpaper_main_dir
 
+# Arch Linux: Run `sudo pacman -S archlinux-artwork`
 logo_file = "/usr/share/archlinux/icons/archlinux-icon-crystal-256.svg"
 logo_text = "arch<span foreground='#3ba4d8'>linux</span>"
 
@@ -25,17 +26,29 @@ cmd_volume_toggle = ["pactl", "set-sink-mute", "0", "toggle"]
 cmd_volume_decrease = ["pactl", "set-sink-volume", "0", "-2%"]
 cmd_volume_increase = ["pactl", "set-sink-volume", "0", "+2%"]
 
+# Run `ls /sys/class/backlight`
+# or `brightnessctl`
 dev_backlight = "amdgpu_bl0"
+# Run `tail /sys/class/hwmon/hwmon*/name`
+# or `sensors`
 dev_thermal = "acpitz"
+# Run `ls /sys/class/net`
+# or `nmcli device`
 dev_nic = ["wlp1s0", "enp2s0"]
-dev_kdeconnect = "eeeeeeeeeeeeeeee"
+# If you have connected your phone to kdeconnect,
+# set the device ID to show its battery status.
+# Run `kdeconnect-cli --list-devices`
+dev_kdeconnect = None
+# dev_kdeconnect = "eeeeeeeeeeeeeeee"
 
 float_rules = [
     Match(wm_class="firefox", role="Organizer"),
     Match(wm_class="firefox", role="page-info"),
     Match(wm_class="firefox", role="About"),
-    Match(wm_class="Steam"),
+    Match(wm_class="lxqt-config"),
     Match(wm_class="pinentry-qt"),
+    Match(wm_class="scrcpy"),
+    Match(wm_class="Steam"),
 ]
 
 float_config = [
