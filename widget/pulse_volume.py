86e2123aaefc1791a69011e05f814b32867596a1
rydesun <rydesun@gmail.com>
2024-04-20 10:59:12 +0800

    Fix volume icon

--- widget/pulse_volume.py
+++ widget/pulse_volume.py
@@ -1,4 +1,5 @@
 from libqtile.widget.pulse_volume import PulseVolume as _PulseVolume
+from libqtile.widget.pulse_volume import pulse
 
 from .base import Box
 from .decoration import inject_decorations
@@ -20,16 +21,24 @@ class PulseVolume(Box, _PulseVolume):
         self.add_defaults(self.defaults)
 
     def _update_drawer(self):
+        if self.volume is None:
+            return
         if self.volume < 0:
             volume = self.mute_text
-            if self.default_sink_name in self.dev_headphone_sinks:
+            if self.is_headphone():
                 icon = self.icon_headphone_mute
             else:
                 icon = self.icon_mute
         else:
             volume = self.volume
-            if self.default_sink_name in self.dev_headphone_sinks:
+            if self.is_headphone():
                 icon = self.icon_headphone
             else:
                 icon = self.icon
-        self.text = '{}{}'.format(icon, volume)
+        self.text = f'{icon}{volume}'
+
+    def is_headphone(self) -> bool:
+        if not pulse.default_sink_name or not pulse.default_sink:
+            return False
+        return pulse.default_sink_name in self.dev_headphone_sinks \
+            or "headphone" in pulse.default_sink.port_active.name.lower()
